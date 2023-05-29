7cc2fa1ebf3f191d914141e904bdd5ede35dcd95
ianK-code <kian99564@gmail.com>
2023-05-29 15:49:06 +0300

    Auth pages done

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import ellipse from "./../assets/ellipse.png";
+import ellipse2 from "./../assets/ellipse2.png";
+import arrowstwo from "./../assets/arrowstwo.png";
 import { useSelector, useDispatch } from "react-redux";
 import Navbar from "../components/Navbar";
 import { setServiceProvider, setClient } from "./../features/signupSlice";
@@ -6,36 +9,39 @@ import { setServiceProvider, setClient } from "./../features/signupSlice";
 const Register = () => {
   const dispatch = useDispatch();
   const store = useSelector((store) => store.signup);
-  console.log(store);
   const [step, setStep] = useState(1);
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [isServiceProvider, setIsServiceProvider] = useState(false);
   const [isClient, setIsClient] = useState(false);
 
   const nextStep = () => {
-    setStep((step) => step + 1);
+    setStep(step + 1);
   };
 
   const prevStep = () => {
-    setStep((step) => step - 1);
+    setStep(step - 1);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("Registration submitted:", {
+    const formCredentials = {
       firstName,
       lastName,
       email,
       password,
-    });
+      isClient: store.isClient,
+      isServiceProvider: store.isServiceProvider,
+    };
     // Reset form fields
     setFirstName("");
     setLastName("");
     setEmail("");
     setPassword("");
+    console.log("Registration submitted:", formCredentials);
   };
 
   const renderForm = () => {
@@ -47,7 +53,7 @@ const Register = () => {
 
             {/* For service provider */}
             <div
-              className="flex border-2 border-black rounded cursor-pointer"
+              className="flex border-2 border-black rounded cursor-pointer py-7 px-10 justify-evenly"
               onClick={() => {
                 dispatch(setServiceProvider(true));
                 dispatch(setClient(false));
@@ -55,47 +61,56 @@ const Register = () => {
                 nextStep();
               }}
             >
-              <img src="" alt="hand" />
+              <img
+                src={ellipse}
+                alt="ellipse.png"
+                style={{
+                  width: "40px",
+                }}
+              />
               <div>
                 <h3>Offer services</h3>
                 <span>Be a Service Provider and Shine!</span>
               </div>
               <div> </div>
             </div>
-            <div className="flex">
-              <img src="" alt="arrow.png" />
+            <div className="flex gap-2 justify-center items-center my-5">
+              <img src={arrowstwo} alt="arrow.png" />
               <span>Connect</span>
             </div>
 
             {/* For client */}
             <div
-              className="flex border-2 border-black rounded cursor-pointer"
+              className="flex border-2 border-black rounded cursor-pointer py-7 px-10 justify-evenly"
               onClick={() => {
                 dispatch(setServiceProvider(false));
                 dispatch(setClient(true));
                 nextStep();
               }}
             >
-              <img src="" alt="search.png" />
+              <img
+                src={ellipse2}
+                alt="search.png"
+                style={{
+                  width: "40px",
+                }}
+              />
               <div>
                 <h3>Discover Services</h3>
                 <span>Find the Perfect Services for Your Needs!</span>
               </div>
               <div> </div>
             </div>
-
-            {/* <button type="button" onClick={nextStep}>
-              Next
-            </button> */}
           </div>
         );
       case 2:
         return (
           <div>
             <h2>Step 2: Personal Information</h2>
-            <form className="flex flex-col">
+            <form className="flex flex-col border-2 border-black rounded-md py-10 px-14">
               <label>First Name:</label>
               <input
+                className="border-2 border-black rounded pl-4"
                 type="text"
                 value={firstName}
                 onChange={(e) => setFirstName(e.target.value)}
@@ -103,17 +118,20 @@ const Register = () => {
 
               <label>Last Name:</label>
               <input
+                className="border-2 border-black rounded pl-4"
                 type="text"
                 value={lastName}
                 onChange={(e) => setLastName(e.target.value)}
               />
 
-              <button type="button" onClick={prevStep}>
-                Previous
-              </button>
-              <button type="button" onClick={nextStep}>
-                Next
-              </button>
+              <div className="flex flex-col gap-3 mt-3">
+                <button onClick={prevStep} className="hover:text-green-600">
+                  Previous
+                </button>
+                <button onClick={nextStep} className="hover:text-green-600">
+                  Next
+                </button>
+              </div>
             </form>
           </div>
         );
@@ -121,9 +139,11 @@ const Register = () => {
         return (
           <div>
             <h2>Step 3: Account Information</h2>
-            <form className="flex flex-col">
+            <form className="flex flex-col border-2 border-black rounded-md py-10 px-14">
               <label>Email:</label>
               <input
+                required
+                className="border-2 border-black rounded pl-4"
                 type="email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
@@ -131,16 +151,31 @@ const Register = () => {
 
               <label>Password:</label>
               <input
+                required
+                className="border-2 border-black rounded pl-4"
                 type="password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
               />
 
-              <div>
-                <button type="button" onClick={prevStep}>
+              <label>Confirm Password:</label>
+              <input
+                required
+                className="border-2 border-black rounded pl-4"
+                type="password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+
+              <div className="flex flex-col gap-3 mt-3">
+                <button onClick={prevStep} className="hover:text-green-600">
                   Previous
                 </button>
-                <button type="submit" onClick={handleSubmit}>
+                <button
+                  className="border-2 border-green-900 rounded bg-green-900 text-white font-bold outline-none p-2 px-3"
+                  type="submit"
+                  onClick={handleSubmit}
+                >
                   Register
                 </button>
               </div>

b6bf86fcec6bca3129607c3f27227b026298d417
ntwigamartin <ntwiga.martin@gmail.com>
2023-05-30 09:37:45 +0300

    customer homepage done

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Store from './redux/Store';
+import { Provider } from 'react-redux';
+import HomePage from './components/cust_homepage/HomePage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={Store}>
+      <HomePage />
+    </Provider>
   </React.StrictMode>
 );
 

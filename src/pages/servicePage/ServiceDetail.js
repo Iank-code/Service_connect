9581c373653d3651610597585a1bf3600bcd4428
ianK-code <kian99564@gmail.com>
2023-06-04 12:00:37 +0300

    Added service cart reducer

--- src/pages/servicePage/ServiceDetail.js
+++ src/pages/servicePage/ServiceDetail.js
@@ -13,7 +13,7 @@ import checkMark from "./../../assets/checkMark.png";
 
 function ServiceDetail() {
   const [value, onChange] = useState(new Date());
-  if(value) console.log(value)
+  if (value) console.log(value);
 
   return (
     <div className="h-screen">
@@ -130,12 +130,65 @@ function ServiceDetail() {
         </div>
       </div>
 
-      <div className="flex py-10 justify-center items-center">
-        {/* For calender */}
-        <div className='w-96'>
-          <Calendar onChange={onChange} value={value} />
+      <div className="flex py-14 my-14 gap-14 justify-center items-center">
+        {/* For calender and rating section */}
+        <div>
+          <h2 className="font-bold my-6">Select best time for cleaning</h2>
+          <div className="w-96 border-2 rounded-md p-14 box-style">
+            <Calendar onChange={onChange} value={value} />
+          </div>
         </div>
-        <div></div>
+        <div className="border-2 rounded-md p-14 box-style">
+          <div className="flex flex-col">
+            <h2>Booking Summary</h2>
+            <hr
+              style={{
+                width: "50%",
+                border: "1px solid black",
+              }}
+            />
+          </div>
+          <div>
+            {/* For price */}
+            <span className="font-bold">Ksh. 500</span>
+            {/* For service being offered */}
+            <span>Home Cleaning</span>
+            {/* For average star rating */}
+            <span>
+              4.5 {"*"} {"10 reviews"}
+            </span>
+          </div>
+
+          {/* Shows service date and time */}
+          <div className="flex gap-4 justify-between">
+            <div>
+              <img src="" alt="" />
+              <div>
+                <h3 className="font-bold">Date</h3>
+                <span>5/20/2023</span>
+              </div>
+            </div>
+            <div>
+              <div>
+                <h3 className="font-bold">Time</h3>
+                <span>10.00am</span>
+              </div>
+            </div>
+          </div>
+
+          {/* Showing work to the service provider */}
+          <div className="border-2 border-black rounded">
+            <div className="flex flex-col">
+              <h3 className="font-bold">Services</h3>
+              <span>3 selected</span>
+            </div>
+
+            
+          </div>
+          <button className="bg-green-800 text-white rounded outline-none py-1 px-4">
+            Confirm Request
+          </button>
+        </div>
       </div>
     </div>
   );

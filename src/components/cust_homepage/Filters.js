b24afbe341776766a7386a7b0c34bf02c9411686
barsu5489 <ebarsulai@gmail.com>
2023-06-22 03:50:48 +0300

    home adding search functionality

--- src/components/cust_homepage/Filters.js
+++ src/components/cust_homepage/Filters.js
@@ -7,40 +7,40 @@ function Filters() {
 
     return (
             <div className="flex flex-row justify-between px-20">
-                <div onClick={() => dispatch(setSearchTerm(""))} className='hover:bg-gray-400 cursor-pointer'>
-                    <img src='/icons/All Categories.png' alt='Cleaning'className='max-h-24'/>
-                    <span>All Categories</span>
+                <div onClick={() => dispatch(setSearchTerm(""))} className='rounded-lg p-4 hover:bg-gray-300 cursor-pointer'>
+                    <img src='/icons/All Categories.png' alt='Cleaning' className='h-10'/>
+                    <span>All </span>
                 </div>
-                <div onClick={() => dispatch(setSearchTerm("Cleaners"))} className='hover:bg-gray-400 cursor-pointer'>
-                    <img src='/icons/Cleaning.png' alt='Cleaning'/>
+                <div onClick={() => dispatch(setSearchTerm("Cleaners"))} className='rounded-lg p-4 hover:bg-gray-300 cursor-pointer'>
+                    <img src='/icons/Cleaning.png' alt='Cleaning' className='h-10'/>
                     <span>Cleaners</span>
                 </div>
-                <div onClick={() => dispatch(setSearchTerm("Electrician"))} className='hover:bg-gray-400 cursor-pointer'>
-                    <img src='/icons/Tester.png' alt='Electrical tools'/>
+                <div onClick={() => dispatch(setSearchTerm("Electrician"))} className='rounded-lg p-4 hover:bg-gray-300 cursor-pointer'>
+                    <img src='/icons/Tester.png' alt='Electrical tools' className='h-10'/>
                     <span>Electricians</span>
                 </div>
-                <div onClick={() => dispatch(setSearchTerm("Laundry"))} className='hover:bg-gray-400 cursor-pointer'>
-                    <img src='/icons/Washer.png' alt='Washing machine'/>
+                <div onClick={() => dispatch(setSearchTerm("Laundry"))} className='rounded-lg p-4 hover:bg-gray-300 cursor-pointer'>
+                    <img src='/icons/Washer.png' alt='Washing machine' className='h-10'/>
                     <span>Laundry</span>
                 </div>
-                <div onClick={() => dispatch(setSearchTerm("Movers"))} className='hover:bg-gray-400 cursor-pointer'>
-                    <img src='/icons/Moving truck.png' alt='Truck icon'/>
+                <div onClick={() => dispatch(setSearchTerm("Movers"))} className='rounded-lg p-4 hover:bg-gray-300 cursor-pointer'>
+                    <img src='/icons/Moving truck.png' alt='Truck icon' className='h-10'/>
                     <span>Movers</span>
                 </div>
-                <div onClick={() => dispatch(setSearchTerm("Maintenance"))} className='hover:bg-gray-400 cursor-pointer'>
-                    <img src='/icons/Tools.png' alt='Maintenance tools'/>
+                <div onClick={() => dispatch(setSearchTerm("Maintenance"))} className='rounded-lg p-4 hover:bg-gray-300 cursor-pointer'>
+                    <img src='/icons/Tools.png' alt='Maintenance tools' className='h-10'/>
                     <span>Maintenance</span>
                 </div>
-                <div onClick={() => dispatch(setSearchTerm("Gardening"))} className='hover:bg-gray-400 cursor-pointer'>
-                    <img src='/icons/Gardening tools.png' alt='Gardening tools'/>
+                <div onClick={() => dispatch(setSearchTerm("Gardening"))} className='rounded-lg p-4 hover:bg-gray-300 cursor-pointer'>
+                    <img src='/icons/Gardening tools.png' alt='Gardening tools' className='h-10'/>
                     <span>Gardening</span>
                 </div>
-                <div onClick={() => dispatch(setSearchTerm("Plumbing"))} className='hover:bg-gray-400 cursor-pointer'>
-                    <img src='/icons/Plumber.png' alt='Plumbing tools'/>
+                <div onClick={() => dispatch(setSearchTerm("Plumbing"))} className='rounded-lg p-4 hover:bg-gray-300 cursor-pointer'>
+                    <img src='/icons/Plumber.png' alt='Plumbing tools' className='h-10'/>
                     <span>Plumbing</span>
                 </div>
-                <div onClick={() => dispatch(setSearchTerm("Remodeling"))} className='hover:bg-gray-400 cursor-pointer'>
-                    <img src='/icons/House.png' alt='house icon'/>
+                <div onClick={() => dispatch(setSearchTerm("Remodeling"))} className='rounded-lg p-4 hover:bg-gray-300 cursor-pointer'>
+                    <img src='/icons/House.png' alt='house icon' className='h-10'/>
                     <span>Remodeling</span>
                 </div>
             </div>
--- src/components/cust_homepage/HomePage.js
+++ src/components/cust_homepage/HomePage.js
@@ -6,6 +6,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { setServicesData } from '../../redux/ServicesData';
 import Navbar from '../Navbar';
 
+
 function HomePage() {
     const { servicesData} = useSelector(state => state.services);
     const { searchTerm} = useSelector(state => state.searchTerm);
@@ -29,20 +30,21 @@ function HomePage() {
                 <SearchItem/>
                 <Filters/>
                 <div className='flex flex-wrap justify-between px-24'>
-                    {
-                        filteredServices.map((service) => 
-                        <div key={service.id} className='m-8'>
-                            <img src={service.image_url} alt={service.service_name}/>
-                            <span className='flex justify-between mt-2.5'>
-                                <span>{service.service_name}</span>
-                                <span className='flex'>
-                                    <img src="/images/Star.png" alt="" className='max-h-5 mr-2'/>
-                                    <span>{service.rating}</span>
-                                </span>
-                            </span>
-                        </div>)
-                    }
-                </div>
+  {
+    filteredServices.map((service) => 
+      <div key={service.id} className='m-8 hover:cursor-pointer'>
+        <img src={service.image_url} alt={service.service_name} className="w-64 h-36 object-cover"/>
+        <span className='flex justify-between mt-2.5'>
+          <span>{service.service_name}</span>
+          <span className='flex'>
+            <img src="/images/Star.png" alt="" className='max-h-5 mr-2'/>
+            <span>{service.rating}</span>
+          </span>
+        </span>
+      </div>
+    )
+  }
+</div>
             </div>
     )
 };

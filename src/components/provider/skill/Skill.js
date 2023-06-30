e2d3fef2509c5059f8e4ac3c7d3fac02b3303aa2
barsu5489 <ebarsulai@gmail.com>
2023-06-30 20:44:12 +0300

    upload update

--- src/components/provider/skill/Skill.js
+++ src/components/provider/skill/Skill.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Skill.css";
 import { useNavigate } from "react-router-dom";
 function Skill() {
   const navigate = useNavigate();
+  const formItem = new FormData();
+const [name, setName] = useState()
+const [price, setPrice] = useState()
+const [file, setFile] = useState()
+  formItem.append("name", name);
+  formItem.append("price", price);
+  formItem.append("file", file);
+
+  function handleSubmit(e){
+    e.preventEfault()
+    console.log(formItem)
+  }
   return (
     <div>
       <div className="skill-container">
@@ -13,21 +25,30 @@ function Skill() {
             <p>Add name or title and price for each sub-service</p>
             <p>Stand out and entice customers to choose your services.</p>
           </div>
-          <div class="upload-sub-service-container">
+          <form class="upload-sub-service-container">
             <label class="upload-box" for="upload">
               <i class="upload-icon fas fa-cloud-upload-alt"></i>
 
               <span class="upload-text">Choose photos or drag it here</span>
             </label>
-
-            <input id="upload" type="file" accept="image/*" multiple />
+            <input
+            id="upload"
+                type="file"
+                accept="image/*"
+          
+                onChange={(e) => {
+                  setFile(e.target.files[0]);
+                }}
+              
+                multiple/>
+            {/* <input id="upload" type="file" accept="image/*" multiple /> */}
             <div class="image-info">
               <div class="image-name">
                 <label for="name">Name:</label>
                 <input
-                  id="name"
-                  type="text"
-                  placeholder="Enter a name for your photo"
+                 type="text"
+                 value={name}
+                 onChange={(e) => setName(e.target.value)}
                 />
               </div>
 
@@ -35,13 +56,15 @@ function Skill() {
                 <label for="price">Price:</label>
 
                 <input
-                  id="price"
-                  type="number"
+                 type="text"
+                 value={price}
+                 onChange={(e) => setPrice(e.target.value)}
                   placeholder="Enter a price for this "
                 />
               </div>
             </div>
-          </div>
+            <button className="back" type="submit"onClick={handleSubmit}>upload</button>
+          </form>
         </div>
       </div>
       <div className="next-back-btn">
